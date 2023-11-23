81b000173e5dd9f150ce064a3281105f71908755
Shabero <tokmokiuca@gmail.com>
2023-11-24 00:07:14 +0600

    Search pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from "./Pages/HomePage";
 import {Route, Routes} from "react-router-dom";
 import SearchPage from "./Pages/SearchPage";
+import MealPage from "./Pages/MealPage";
 
 const  App = () => {
     return (
         <Routes>
             <Route path={"/"} element={<HomePage /> }/>
             <Route path={"/search"} element={<SearchPage />} />
+            <Route path={"/meal/:idMeal"} element={<MealPage />}/>
         </Routes>
     )
 }
--- src/Components/MealCard.js
+++ src/Components/MealCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const MealCard = ({meal}) => {
     return (
         <div className={'box'}>
             <img src={meal?.strMealThumb} alt=""/>
-            <h5 className={'title'}>{meal?.strMeal}</h5>
+            <h5 className={'title'}>
+                <Link to={`/meal/${meal?.idMeal}`}>
+                    {meal?.strMeal}
+                </Link>
+            </h5>
             <p className={'description'}>{meal?.strCategory}</p>
         </div>
     )
